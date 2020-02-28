4d1aa54df60ef5834e9e249847e25abff03c2a78
Jignesh45 <jignesh.wala1994@gmail.com>
2020-02-28 17:56:07 +0530

    rest api updated

--- src/test/java/qaclickacademy/com/RestAPITest.java
+++ src/test/java/qaclickacademy/com/RestAPITest.java
@@ -7,6 +7,8 @@ public class RestAPITest {
 	@Test
 	public void PostJira() {
 		System.out.println("PostJira");
+		System.out.println("PostJira1");
+		System.out.println("PostJira2");
 	}
 	
 	@Test
